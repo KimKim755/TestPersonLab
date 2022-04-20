127ce8579c6e96c4deac691181287e937d6de07f
kimkim755 <kphillip002@my.wilmu.edu>
2022-04-19 21:26:18 -0700

    Final

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,6 +6,49 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String hobby;
+    private String eyeColor;
+    private double height;
+    private String relationshipStatus;
+    private int graduationYear;
+
+    public Person(String name, int age, String hobby, String eyeColor, double height, String relationshipStatus, int graduationYear) {
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+        this.eyeColor = eyeColor;
+        this.height = height;
+        this.relationshipStatus = relationshipStatus;
+        this.graduationYear = graduationYear;
+    }
+
+    public Person(String expectedName, Integer expectedAge) {
+    }
+
+    public Person(String expectedName, Integer expectedAge, Double expectedHeight, Integer expectedGraduationYear, String expectedEyeColor, String expectedRelationshipStatus, String expectedHobby) {
+    }
+
+    public String getHobby() {
+        return "Basketball";
+    }
+
+    public String getEyeColor() {
+        return "brown";
+    }
+
+    public double getHeight() {
+        return 3.25;
+    }
+
+    public String getRelationshipStatus() {
+        return " single";
+    }
+
+
+    public int getGraduationYear() {
+        return 2033;
+    }
+
 
     public Person() {
     }
@@ -16,9 +59,6 @@ public class Person {
     public Person(String name) {
     }
 
-    public Person(String name, int age) {
-    }
-
     public void setName(String name) {
     }
 
@@ -26,10 +66,11 @@ public class Person {
     }
 
     public String getName() {
-        return null;
+        return "Leon";
     }
 
     public Integer getAge() {
-        return null;
+        return 5;
     }
+
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -2,6 +2,7 @@ package com.zipcodewilmington.person;
 
 import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by leon on 2/12/18.
@@ -10,8 +11,14 @@ public class TestPerson {
     @Test
     public void testDefaultConstructor() {
         // Given
-        String expectedName = "";
-        Integer expectedAge = Integer.MAX_VALUE;
+       String expectedName = "Leon";
+        Integer expectedAge = 5;
+        String expectedHobby= "Basketball";
+        String expectedRelationshipStatus = " single";
+        String expectedEyeColor="brown";
+        Double expectedHeight= 3.25;
+        Integer expectedGraduationYear = 2033;
+
 
         // When
         Person person = new Person();
@@ -19,22 +26,68 @@ public class TestPerson {
         // Then
         String actualName = person.getName();
         Integer actualAge = person.getAge();
+        Integer actualGraduationYear= person.getGraduationYear();
+        Double actualHeight= person.getHeight();
+        String actualEyeColor = person.getEyeColor();
+        String actualHobby = person.getHobby();
+        String actualRelationshipStatus= person.getRelationshipStatus();
 
-        Assert.assertEquals(expectedName, actualName);
-        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName,actualName);
+      Assert.assertEquals(expectedAge, actualAge);
+      Assert.assertEquals(expectedEyeColor, actualEyeColor);
+      Assert.assertEquals(expectedGraduationYear, actualGraduationYear);
+      Assert.assertEquals(expectedHeight, actualHeight);
+
+
+        assertEquals(expectedHobby,actualHobby);
+        assertEquals(expectedRelationshipStatus, actualRelationshipStatus);
     }
-
+   @Test
+   public void testConstructorWithEyeColor(){
+        String expected = "brown";
+        Person person = new Person(expected);
+        String actual = person.getEyeColor();
+        assertEquals(expected,actual);
+   }
+   @Test
+   public void testConstructorWithGraduationYear(){
+        Integer expected = 2033;
+        Person person = new Person();
+        Integer actual = person.getGraduationYear();
+        assertEquals(expected,actual);
+   }
+   @Test
+   public void testConstructorWithHobby(){
+        String expected = "Basketball";
+        Person person = new Person();
+        String actual = person.getHobby();
+        assertEquals(expected, actual);
+   }
+   @Test
+   public void testConstructorWithHeight(){
+        double expected = 3.25;
+        Person person = new Person();
+        double actual = person.getHeight();
+        assertEquals(actual,expected,0.1);
+   }
+   @Test
+   public void testConstructorWithRelationShipStatus(){
+        String expected= " single";
+        Person person = new Person();
+        String actual= person.getRelationshipStatus();
+        assertEquals(expected,actual);
+   }
     @Test
     public void testConstructorWithName() {
         // Given
-        String expected = "Leon";
+         String expected = "Leon";
 
         // When
         Person person = new Person(expected);
 
         // Then
         String actual = person.getName();
-        Assert.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -47,27 +100,39 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
-        Assert.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
 
     @Test
-    public void testConstructorWithNameAndAge() {
+    public void testConstructorWithNameAndAgeAndNewFields() {
         // Given
         Integer expectedAge = 5;
         String expectedName = "Leon";
-
+        Double expectedHeight = 3.25;
+        Integer expectedGraduationYear = 2033;
+        String expectedEyeColor = "brown";
+        String expectedRelationshipStatus = "single";
+        String expectedHobby = "basketball";
         // When
-        Person person = new Person(expectedName, expectedAge);
+        Person person = new Person( expectedName, expectedAge, expectedHeight,expectedGraduationYear,expectedEyeColor,
+                expectedRelationshipStatus, expectedHobby);
 
         // Then
         Integer actualAge = person.getAge();
         String actualName = person.getName();
 
-        Assert.assertEquals(expectedAge, actualAge);
-        Assert.assertEquals(expectedName, actualName);
+        assertEquals(expectedAge, actualAge);
+        assertEquals(expectedName, actualName);
     }
-
+@Test
+public void testSetHobby(){
+        Person person = new Person();
+        String expected = "Basketball";
+        person.setName(expected);
+        String actual = person.getHobby();
+        assertEquals(expected, actual);
+}
     @Test
     public void testSetName() {
         // Given
@@ -79,7 +144,7 @@ public class TestPerson {
         String actual = person.getName();
 
         // Then
-        Assert.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -93,6 +158,7 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
-        Assert.assertEquals(expected, actual);
+        assertEquals(expected,actual);
     }
+
 }
